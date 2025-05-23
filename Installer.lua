a85cb964c8eff05e8cfbb1c30aa5e361685280f2
MONETMOBILE <110362574+Andrei375577@users.noreply.github.com>
2025-05-23 20:17:31 +0300

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -1,7 +1,7 @@
-local imgui = require 'imgui'
+local imgui = require 'mimgui'
 local lfs = require 'lfs'
 
--- Получаем путь к директории, где находится этот скрипт
+-- Получаем путь к скрипту
 local function getScriptDir()
     local info = debug.getinfo(1, 'S')
     local scriptPath = info.source:sub(2)
@@ -13,8 +13,8 @@ local scriptsPath = scriptDir .. "/scripts"
 
 local scriptFiles = {}
 local selectedFile = nil
-local fileContent = ""
-local isEditing = false
+local fileContent = imgui.new.char[4096]()
+local isEditing = imgui.new.bool(false)
 
 function updateScriptList()
     scriptFiles = {}
@@ -28,43 +28,70 @@ end
 function loadFileContent(filename)
     local f = io.open(scriptsPath .. "/" .. filename, "r")
     if f then
-        fileContent = f:read("*a")
+        local content = f:read("*a") or ""
         f:close()
+        imgui.setValue(fileContent, content)
     else
-        fileContent = ""
+        imgui.setValue(fileContent, "")
     end
 end
 
-function saveFileContent(filename, content)
+function saveFileContent(filename)
     local f = io.open(scriptsPath .. "/" .. filename, "w")
     if f then
-        f:write(content)
+        f:write(imgui.getValue(fileContent))
         f:close()
     end
 end
 
-imgui.OnDraw(function()
-    imgui.Begin("Lua Script Editor")
-    if imgui.Button("Обновить список файлов") then
-        updateScriptList()
-    end
-    imgui.Separator()
-    for i, file in ipairs(scriptFiles) do
-        if imgui.Selectable(file, selectedFile == file) then
-            selectedFile = file
-            loadFileContent(file)
-            isEditing = true
+imgui.OnFrame(
+    function() return true end,
+    function()
+        imgui.Begin("Lua Script Editor")
+        if imgui.Button("Обновить список файлов") then
+            updateScriptList()
         end
-    end
-    imgui.Separator()
-    if selectedFile then
-        imgui.Text("Редактирование: " .. selectedFile)
-        changed, fileContent = imgui.InputTextMultiline("##edit", fileContent, 4096, imgui.ImVec2(500, 300))
-        if imgui.Button("Сохранить") then
-            saveFileContent(selectedFile, fileContent)
+        imgui.Separator()
+        for i, file in ipairs(scriptFiles) do
+            if imgui.Selectable(file, selectedFile == file) then
+                selectedFile = file
+                loadFileContent(file)
+                imgui.setValue(isEditing, false)
+            end
         end
+        imgui.Separator()
+        if selectedFile then
+            imgui.Text("Файл: " .. selectedFile)
+            if not isEditing[0] then
+                if imgui.Button("Edit") then
+                    imgui.setValue(isEditing, true)
+                end
+            else
+                if imgui.Button("Сохранить") then
+                    saveFileContent(selectedFile)
+                    imgui.setValue(isEditing, false)
+                end
+                imgui.SameLine()
+                if imgui.Button("Отмена") then
+                    loadFileContent(selectedFile)
+                    imgui.setValue(isEditing, false)
+                end
+                imgui.InputTextMultiline("##edit", fileContent, imgui.ImVec2(500, 300))
+            end
+        end
+        imgui.End()
     end
-    imgui.End()
-end)
+)
 
-updateScriptList()
+-- Команда активации edit
+function onEditCommand()
+    if selectedFile then
+        imgui.setValue(isEditing, true)
+    end
+end
+
+function main()
+    updateScriptList()
+    sampRegisterChatCommand("edit", onEditCommand) -- Регистрируем команду /edit
+    while true do wait(0) end
+end
